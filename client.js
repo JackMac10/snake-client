39d716bf016edf17479e65d390a7e9e0bf736b8a
JackMac10 <macdonaldjackson0@gmail.com>
2024-02-03 10:13:19 -0800

    client.js implimented and connection function moved

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("welcome, My snakey friend")
+  });
+  
+conn.on("data", (data) => {
+  console.log(data);
+});
+  return conn;
+};
+  module.exports = {connect}
