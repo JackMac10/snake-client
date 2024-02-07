572787756c8596e4461aa488aabb5917a7fcddbf
JackMac10 <macdonaldjackson0@gmail.com>
2024-02-06 21:50:16 -0800

    controls and setupInput removed from constants.js, after request

--- input.js
+++ input.js
@@ -1,8 +1,6 @@
 // define connection
 let connection;
 
-
-// assert input comunication with program
 const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
@@ -14,6 +12,31 @@ const setupInput = (conn) => {
   return stdin;
 };
 
-const {handleUserInput} = require("./constants");
+//controls
+const handleUserInput = function(data) {
+  
+  if (data === '\u0003') {
+    console.log("GoodBye SNAKE");
+    process.exit();
+  }
+  if (data === 'w') {
+    connection.write('Move: up');
+  } else if (data === 's') {
+    connection.write('Move: down');
+  } else if (data === 'a') {
+    connection.write('Move: left');
+  } else if (data === 'd') {
+    connection.write('Move: right');
+  } else if (data === '1') {
+    connection.write('Say: SssSssSSss');
+  } else if (data === '2') {
+    connection.write('Say: AHHHHH');
+  } else if (data === '3') {
+    connection.write('Say: Stay Away');
+  }  else if (data === '4') {
+    connection.write('Say: $*@%!!');
+  }
+};
+
 
 module.exports = {setupInput, handleUserInput};
